7380893d0a40850a280d031a933b876791263948
HunorAtt <hunoratt@gmail.com>
2025-10-25 20:39:23 +0200

    Javadoc grammer fix

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -8,8 +8,8 @@ public class Game {
     private Player player;
 
     /**
-     * Creates instances of players and their tables. After the table is created
-     * its given to the player class.
+     * Creates instances of players and their tables. After the table is created,
+     * it's given to the player class.
      *
      * @param size The size of tables that is given to the players
      * @throws NullPointerException if {@code table} is {@code null}
